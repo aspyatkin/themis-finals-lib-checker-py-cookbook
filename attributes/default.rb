979e2c90f2a638fc2ad4338ebe00f84ab2c5b67c
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-04-26 20:10:08 +0400

    Draft

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+id = 'themis-finals-lib-checker-py'
+
+default[id][:basedir] = '/var/themis/finals/lib/checker-py'
+default[id][:user] = 'vagrant'
+default[id][:group] = 'vagrant'
+default[id][:github_repository] = 'aspyatkin/themis-checker-py'
+default[id][:revision] = 'master'
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'themis-finals-lib-checker-py'
+description 'Installs and configures checker-py'
+version '1.0.0'
+
+recipe 'themis-finals-lib-checker-py', 'Installs and configures checker-py'
+depends 'latest-git', '~> 1.1.6'
+depends 'git2', '~> 1.0.0'
