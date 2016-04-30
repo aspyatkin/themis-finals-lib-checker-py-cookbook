c687adab8993228f64fabd6cd971b59fd725b855
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-04-30 15:56:44 +0400

    Checkout develop by default

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'themis-finals-lib-checker-py'
 description 'Installs and configures checker-py'
-version '1.0.0'
+version '1.0.1'
 
 recipe 'themis-finals-lib-checker-py', 'Installs and configures checker-py'
 depends 'latest-git', '~> 1.1.6'
