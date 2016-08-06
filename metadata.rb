5d9a93bb1b19eb2c2c90dcbdb6a73f2d8d9c6010
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-08-06 14:11:35 +0400

    Replace symbolized keys

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'themis-finals-lib-checker-py'
 description 'Installs and configures checker-py'
-version '1.0.2'
+version '1.0.3'
 
 recipe 'themis-finals-lib-checker-py', 'Installs and configures checker-py'
 depends 'latest-git', '~> 1.1.6'
